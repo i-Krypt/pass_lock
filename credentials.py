f426451761b8b40823273faebb91f088d3a22c5c
i-Krypt <vamay505@gmail.com>
2019-09-02 10:19:34 +0300

    added args to the credentials class

--- credentials.py
+++ credentials.py
@@ -4,4 +4,14 @@ class Credentials:
     class that takes in user credentials.
     '''
 
-    
+    def __init__(self, account, username):
+
+        '''
+        Args:
+        account: New account
+        username: New username
+        '''
+
+        self.account = account
+        self.username = username
+        

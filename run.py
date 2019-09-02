f6b8e853cd80750005e07967c5b5d99adaa1041f
i-Krypt <vamay505@gmail.com>
2019-09-02 16:25:07 +0300

    added a method to save a user

--- run.py
+++ run.py
@@ -1,7 +1,7 @@
 from  user import User
 from credentials import Credentials
 
-
+#create a new user 
 def create_user(username, account, password):
 
     '''
@@ -10,3 +10,12 @@ def create_user(username, account, password):
 
     new_user = User(username, account, password)
     return new_user
+
+
+#save user
+def create_credentials(user):
+    '''
+    function to save credential
+    '''
+
+    credentials.save_credentials()

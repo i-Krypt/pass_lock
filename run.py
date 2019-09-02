375a46014976df91e7af20d795c7dd5c64d60a73
i-Krypt <vamay505@gmail.com>
2019-09-02 15:47:42 +0300

    added a return function in run.py to return a new user

--- run.py
+++ run.py
@@ -5,5 +5,8 @@ from credentials import Credentials
 def create_user(username, account, password):
 
     '''
-    Function to create a new contact
+    Function to create a new user
     '''
+
+    new_user = User(username, account, password)
+    return new_user
